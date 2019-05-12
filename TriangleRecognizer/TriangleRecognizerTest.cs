7012b5399a8d06082e33a50dc455cbd0a726b4b0
Bruce <gkm85663@gmail.com>
2019-05-12 23:30:12 +0800

    Pass test - Three_Edges_sam_but_greater_than_zero_return_right_triangle

--- TriangleRecognizer/TriangleRecognizerTest.cs
+++ TriangleRecognizer/TriangleRecognizerTest.cs
@@ -16,15 +16,46 @@ namespace TriangleRecognizerTest
             Assert.AreEqual("not triangle", GetTriangleIdentificationResult(edge1, edge2, edge3));
         }
 
+        [Test]
+        public void Three_Edges_sam_but_greater_than_zero_return_right_triangle()
+        {
+            const int edge1 = 3;
+            const int edge2 = 3;
+            const int edge3 = 3;
+
+            Assert.AreEqual("regular triangle", GetTriangleIdentificationResult(edge1, edge2, edge3));
+        }
+
         private string GetTriangleIdentificationResult(params int[] edges)
         {
-            return IsTriangle(edges) ? "" : "not triangle";
+            if (IsTriangle(edges))
+            {
+                if (IsRegularTriangle(edges))
+                {
+                    return "regular triangle";
+                }
+            }
+
+            return "not triangle";
+        }
+
+        private bool IsRegularTriangle(int[] edges)
+        {
+            for (var i = 0; i < edges.Length - 1; i++)
+            {
+                if (edges[i] != edges[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         private bool IsTriangle(int[] edges)
         {
             Array.Sort(edges);
-            return edges.Length == 3 && edges.Where(e => e < edges.Max()).Sum() > edges.Max();
+            return edges.Length == 3 && edges.Where(e => e <= edges.Max()).Sum() > edges.Max();
         }
     }
 }
