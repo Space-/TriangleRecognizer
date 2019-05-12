d2e8a48a6d99611d004112e43b3bb78fc6a5ee0b
Bruce <gkm85663@gmail.com>
2019-05-12 22:30:01 +0800

    Pass test - Two_Edges_sum_Less_Than_Max_Edge_return_not_triangle()

--- TriangleRecognizer/TriangleRecognizerTest.cs
+++ TriangleRecognizer/TriangleRecognizerTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace TriangleRecognizerTest
@@ -5,9 +7,24 @@ namespace TriangleRecognizerTest
     public class TriangleRecognizerTest
     {
         [Test]
-        public void Test1()
+        public void Two_Edges_sum_Less_Than_Max_Edge_return_not_triangle()
         {
-            Assert.Pass();
+            const int edge1 = 1;
+            const int edge2 = 2;
+            const int edge3 = 0;
+
+            Assert.AreEqual("not triangle", GetTriangleIdentificationResult(edge1, edge2, edge3));
+        }
+
+        private string GetTriangleIdentificationResult(params int[] edges)
+        {
+            return IsTriangle(edges) ? "not triangle" : "";
+        }
+
+        private bool IsTriangle(int[] edges)
+        {
+            Array.Sort(edges);
+            return edges.Length == 3 || edges.Where(e => e < edges.Max()).Sum() > edges.Max();
         }
     }
 }
