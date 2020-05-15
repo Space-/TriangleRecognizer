ed7c11910f5acfebea48435a866a309d1ae2756b
Bruce <gkm85663@gmail.com>
2020-05-15 22:23:00 +0800

    Refactor other test cases by using GivenTriangleRecognizer method

--- TriangleRecognizer/TriangleRecognizerTest.cs
+++ TriangleRecognizer/TriangleRecognizerTest.cs
@@ -20,36 +20,36 @@ namespace TriangleRecognizerTest
         public void Three_Edges_same_but_greater_than_zero_return_regular_triangle()
         {
             GivenEdges(3, 3, 3);
+            GivenTriangleRecognizer();
 
-            var triangleRecognizer = new TriangleRecognizer(3, 3, 3);
-            Assert.AreEqual("regular triangle,isosceles triangle,acute triangle", triangleRecognizer.GetTriangleIdentificationResult());
+            Assert.AreEqual("regular triangle,isosceles triangle,acute triangle", _triangleRecognizer.GetTriangleIdentificationResult());
         }
 
         [Test]
         public void Pythagorean_theorem_return_right_triangle()
         {
             GivenEdges(3, 4, 5);
+            GivenTriangleRecognizer();
 
-            var triangleRecognizer = new TriangleRecognizer(3, 4, 5);
-            Assert.AreEqual("right triangle", triangleRecognizer.GetTriangleIdentificationResult());
+            Assert.AreEqual("right triangle", _triangleRecognizer.GetTriangleIdentificationResult());
         }
 
         [Test]
         public void Two_Edges_sum_Less_Than_Max_Edge_and_all_edges_not_zero_return_obtuse_triangle()
         {
             GivenEdges(9, 11, 17);
+            GivenTriangleRecognizer();
 
-            var triangleRecognizer = new TriangleRecognizer(9, 11, 17);
-            Assert.AreEqual("obtuse triangle", triangleRecognizer.GetTriangleIdentificationResult());
+            Assert.AreEqual("obtuse triangle", _triangleRecognizer.GetTriangleIdentificationResult());
         }
 
         [Test]
         public void Two_Edges_sum_Greater_Than_Max_Edge_and_all_edges_not_zero_return_acute_triangle()
         {
             GivenEdges(5, 6, 7);
+            GivenTriangleRecognizer();
 
-            var triangleRecognizer = new TriangleRecognizer(5, 6, 7);
-            Assert.AreEqual("acute triangle", triangleRecognizer.GetTriangleIdentificationResult());
+            Assert.AreEqual("acute triangle", _triangleRecognizer.GetTriangleIdentificationResult());
         }
 
         [TestCase(5, 4, 4, "isosceles triangle,acute triangle")]
@@ -58,8 +58,10 @@ namespace TriangleRecognizerTest
         [TestCase(3, 3, 5, "isosceles triangle,obtuse triangle")]
         public void Two_Edges_same_and_its_sum_greater_than_third_edge_and_all_edges_not_zero_return_isosceles_triangle(int edge1, int edge2, int edge3, string triangleType)
         {
-            var triangleRecognizer = new TriangleRecognizer(edge1, edge2, edge3);
-            Assert.AreEqual(triangleType, triangleRecognizer.GetTriangleIdentificationResult());
+            GivenEdges(edge1, edge2, edge3);
+            GivenTriangleRecognizer();
+
+            Assert.AreEqual(triangleType, _triangleRecognizer.GetTriangleIdentificationResult());
         }
 
         private void GivenEdges(params int[] edges)
